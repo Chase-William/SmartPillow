a987eb0bf1e9b7d69b2433ce059a00f6bc3ff499
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-06-15 23:04:35 -0400

    applied skia gradient to homePage's background

--- SmartPillow/SmartPillow/Pages/HomePage.xaml.cs
+++ SmartPillow/SmartPillow/Pages/HomePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,6 +18,28 @@ namespace SmartPillow.Pages
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void SKCanvas_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            using (SKPaint paint = new SKPaint())
+            {
+                // Create linear gradient from upper-left to lower-right
+                paint.Shader = SKShader.CreateLinearGradient(
+                                    new SKPoint(0, 0),
+                                    new SKPoint(e.Info.Width, e.Info.Height),
+                                    new SKColor[] { ((Color)App.Current.Resources[App.Keys.GradientBlueKey]).ToSKColor(), ((Color)App.Current.Resources[App.Keys.GradientPurpKey]).ToSKColor() },
+                                    null,
+                                    SKShaderTileMode.Repeat);
+
+                // Draw the gradient on the rectangle
+                canvas.DrawRect(0, 0, e.Info.Width, e.Info.Height, paint);
+            }
         }
     }
 }
