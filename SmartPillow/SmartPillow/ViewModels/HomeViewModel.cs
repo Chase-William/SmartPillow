ed7819927c96a8b454f5e59c8aa5d8b5fe592c63
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-06-16 20:05:36 -0400

    Create HomeViewModel.cs
    set this viewModel up for HomePage to bind

---
+++ SmartPillow/SmartPillow/ViewModels/HomeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SmartPillow.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        private string profileImage;
+
+        public string ProfileImage
+        {
+            get { return profileImage; }
+            set { profileImage = value; }
+        }
+
+        public HomeViewModel()
+        {
+            ProfileImage = "Zack.png";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
