f5e366210e9715942cd0625c439351a34b181504
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-06-17 20:28:52 -0400

    Create User.cs

---
+++ SmartPillowLib/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartPillowLib.Models
+{
+    public class User
+    {
+        /// <summary>
+        ///     Information of user that is currently logged in.
+        /// </summary>
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        ///     ID of smart pillow device registered to user.
+        /// </summary>
+        public string SmartPillowDeviceID { get; set; }
+
+    }
+}
