005f5352c629492da14f3a7462976349f286afe5
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-06-18 01:22:18 -0400

    Create UserInformation.cs
    creating user object for test

---
+++ SmartPillowLib/UserInformation.cs
@@ -0,0 +1,27 @@
+﻿using SmartPillowLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartPillowLib
+{
+    public class UserInformation
+    {
+        public static bool IsUserLogged { get; set; }
+        public User User { get; set; }
+        public UserInformation()
+        {
+            //testing user
+            var user = new User()
+            {
+                FirstName = "Mark",
+                LastName = "Zuckerberg",
+                Image = "Zack.png",
+                Email = "Email@gmail.com",
+                PhoneNumber = "585-585-5858"
+            };
+
+            User = user;
+        }
+    }
+}
