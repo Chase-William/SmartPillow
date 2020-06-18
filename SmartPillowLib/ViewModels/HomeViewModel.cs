a1bce4b8a0e7b67816052ba6b47b50eb003ce3a7
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-06-17 20:33:16 -0400

    Update HomeViewModel.cs
    .

--- SmartPillowLib/ViewModels/HomeViewModel.cs
+++ SmartPillowLib/ViewModels/HomeViewModel.cs
@@ -3,12 +3,37 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using SmartPillow;
+using SmartPillowLib.Models;
 
 namespace SmartPillow.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
         private string profileImage;
+        private User user;
+        private bool isUserLogged;
+
+        public INavigation Navigation { get; set; }
+
+        //public bool IsUserLogged
+        //{
+        //    get { return isUserLogged; }
+        //    set { isUserLogged = value; }
+        //}
+
+        public User User
+        {
+            get { return user; }
+            set 
+            { 
+                user = value;
+                NotifyPropertyChanged();
+            }
+        }
+
 
         public string ProfileImage
         {
@@ -18,6 +43,15 @@ namespace SmartPillow.ViewModels
                 NotifyPropertyChanged();
             }
         }
+
+        //public ICommand UserCommand => new Command(async () =>
+        //{
+        //    if (IsUserLogged == true)
+        //        await Navigation.PushAsync(new ProfilePage());
+        //    else
+        //        await Navigation.PushAsync(new LoginPage());
+
+        //});
 
         public HomeViewModel()
         {
