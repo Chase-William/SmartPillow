ad7136fa577e500735429d418fc524efe40292ae
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-06-19 17:08:02 -0400

    Create ProfilePage.xaml.cs

---
+++ SmartPillow/SmartPillow/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SkiaSharp.Views.Forms;
+using SmartPillow.Util;
+using SmartPillowLib.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartPillow.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfileViewModel VM => (ProfileViewModel)BindingContext;
+        public ProfilePage()
+        {
+            InitializeComponent();
+
+            ProfileFrame.PopProfile += async delegate
+            {
+                Content.BackgroundColor = Color.Transparent;
+                await this.Navigation.PopModalAsync();
+            };
+        }
+
+        //private void SKCanvas_PaintSurface(object sender, SKPaintSurfaceEventArgs e) => Painter.PaintGradientBG(e);
+    }
+}
