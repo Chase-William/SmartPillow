7a42ee1986278cbb356b2f1db107342c84f54dab
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-06-19 17:08:07 -0400

    Create ProfileViewModel.cs

---
+++ SmartPillowLib/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SmartPillowLib.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public event Action CloseFrame;
+
+        public ICommand LogoutCommand => new Command(() =>
+        {
+            CloseFrame?.Invoke();
+        });
+    }
+}
